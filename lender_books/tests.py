4221c5b64381a406e9f5a162bc863c808a077b9e
chris turner <-v>
2018-09-18 22:05:08 -0700

    tests
    still not working

--- lender_books/tests.py
+++ lender_books/tests.py
@@ -18,18 +18,20 @@ class TestBookModel(TestCase):
          self.assertEqual(book.author, 'Wat Stick')
 
 
-#  class TestNoteViews(TestCase):
-#     def setUp(self):
-#         self.request = RequestFactory()
-#         self.note_one = Note.objects.create(title='blarp', description='wat stick')
-#         self.note_two = Note.objects.create(title='Gnarf', description='wat dat')
-#      def test_note_detail_view(self):
-#         from .views import notes_detail_view
-#         request = self.request.get('')
-#         response = notes_detail_view(request, f'{self.note_one.id}')
-#         self.assertIn(b'wat stick', response.content)
-#      def test_note_detail_status(self):
-#         from .views import notes_detail_view
-#         request = self.request.get('')
-#         response = notes_detail_view(request, f'{self.note_one.id}')
-#         self.assertEqual(200, response.status_code)
+class TestBookViews(TestCase):
+    def setup(self):
+        self.request = RequestFactory()
+        self.book_1 = Book.objects.create(isbn='4444', title='Feed the Cat', author='Sue Me', year='1992')
+        self.book_2 = Book.objects.create(isbn='4544' title='Wings are Wild', author='June Roth', year='1985')
+
+    def test_book_detail_view(self):
+        from .views import books_detail_view
+        request = self.request.get('')
+        response = books_detail_view(request, f'{self.book_1.id}')
+        self.assertIn(b'Feed the Cat', response.content)
+
+    def test_book_detail_status(self):
+        from .views import books_detail_view
+        request = self.request.get('')
+        response = books_detail_view(request, f'{self.book_2.id}')
+        self.assertEqual(200, response.status_code)
